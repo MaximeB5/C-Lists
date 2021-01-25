e24972bde4f9472f76e9e855068b0433ed68fdfd
MaximeB5 <belavalmax@sfr.fr>
2021-01-25 15:52:37 +0100

    add functional setup

---
+++ src/main.c
@@ -0,0 +1,15 @@
+// My Includes
+	// None for the moment.
+
+// Includes
+#include <stdio.h>
+
+// My Defines
+	// None for the moment.
+
+int main()
+{
+	printf("Hello from C-Lists.\n");
+
+	return(0);
+}
